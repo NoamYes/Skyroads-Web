a0a1dc66a01b6f3a73d24e60af73d98725acb3a3
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-02-18 21:06:56 +0200

    add background

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 1440;
+        this.canvas.height = 700;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
