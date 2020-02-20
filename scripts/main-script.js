011aba7c2e434b7b912fff29520d10b0d75287a7
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-02-20 10:43:02 +0200

    make block of certain dimensions

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -2,6 +2,7 @@
 
 function startGame() {
     myGameArea.start();
+    myRoadArea.init();
 }
 
 var myGameArea = {
@@ -13,3 +14,51 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 }
+
+class Block {
+    constructor(width_, height_, x0_, y0_) {
+        this.height = height_;
+        this.width = width_;
+        this.x0 = x0_;
+        this.y0 = y0_;
+        this.lines = [];
+        this.lineblocks();
+    }
+
+    lineblocks() {
+        for (let i=0; i<this.height; i+=1) {
+        let new_line = new block_line(this.width, this.x0, this.y0+i);
+         this.lines.push(new_line);
+        }
+      }
+
+      block_init(color) {
+        for (let i=0; i<this.height; i+=1) {
+            this.lines[i].init_line(color);
+            }
+      }
+}
+
+class block_line {
+    constructor(width_, x0_, y0_) {
+        this.width = width_;
+        this.x0 = x0_;
+        this.y0 = y0_;
+
+    }
+    init_line(color) {
+        this.context = myGameArea.context;
+        this.context.fillstyle = color;
+        this.context.fillRect(this.x0, this.y0, this.width, 1);
+    }
+}
+
+
+var myRoadArea = {
+    mainBlock : new Block(90, 200, 100, 100),
+     init : function() {
+        
+     this.mainBlock.block_init("red");
+     }
+}
+
