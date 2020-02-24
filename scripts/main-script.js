542e58514f72e3bf8e909c2306c4d7820c4d27e8
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-02-25 00:06:28 +0200

    add spaceship icon

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,6 +1,8 @@
 'use strict';
+var mySpaceship;
 
 function startGame() {
+    mySpaceship = new component(100, 100, "images/Fighter_jet.png", 500, 700, "image");
     myGameArea.start();
     myRoadArea.initRoad();
     myRoadArea.updateRoad();
@@ -21,6 +23,39 @@ var myGameArea = {
     },
 }
 
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        let ctx = myGameArea.context;
+        if (type == "image") {            
+            // this.image.onload = function(){
+                myGameArea.context.drawImage(this.image, 
+                    this.x, 
+                    this.y,
+                    this.width, this.height);
+                
+            // }
+
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }
+}
 
 class Block {
     constructor(width_, height_, row, column, color, roadHeight) {
@@ -149,6 +184,7 @@ var myRoadArea = {
                 }
                 }
         }
+        mySpaceship.update();
 
     },
 
